1d32ae2fb1043a2d75827aa65c96f51002f88a66
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-17 12:48:26 +0400

    Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Heading from "./components/Heading";
 import Oscar from "./components/Oscar";
 import Button from "./components/Button";
 import Input from "./components/Input";
+import { Container } from "./components/Container";
 
 function App() {
   // const personName = {
@@ -44,7 +45,7 @@ function App() {
         Awards goes to Bruce Wayne
       </Heading>
       </Oscar>
-      <Greet name="Saqi" isLoggedIn={false} /> */}
+      <Greet name="Saqi" isLoggedIn={false} />
       <Button 
       handleClick={(event, id) => {
         console.log('Button Clicked', event, id)
@@ -55,7 +56,8 @@ function App() {
       handleChange={(event) => {
         console.log('Value changed', event)
       }}
-      /> 
+      /> */}
+      <Container styles={{ border: '1px solid red', padding: '4px', color: 'green'}}/>
     </>
   );
 }
