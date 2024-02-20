6cba3c448df81a87444292c03174ae0723aedc98
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-20 12:36:02 +0400

    Wrapping HTML Elements

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import { Profile } from "./components/auth/Profile";
 import { List } from "./components/generics/List";
 import { RandomNumber } from "./components/restriction/RandomNumber";
 import { Toast } from "./components/templateliterals/Toast";
+import { CustomButton } from "./components/html/Button";
 
 function App() {
   // const personName = {
@@ -109,8 +110,11 @@ function App() {
         ]}
         onClick={item => console.log(item)}
       />
-      <RandomNumber value={10} isPositive /> */}
-      <Toast position='center' />
+      <RandomNumber value={10} isPositive />
+      <Toast position='center' /> */}
+      <CustomButton variant='primary' onClick={() => console.log('Clicked')}>
+        Button Label
+      </CustomButton>
     </>
   );
 }
