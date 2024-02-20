e55351236adbd6f1607052295fdcdc726dca4666
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-20 13:05:00 +0400

    Polymorphic Components

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import { List } from "./components/generics/List";
 import { RandomNumber } from "./components/restriction/RandomNumber";
 import { Toast } from "./components/templateliterals/Toast";
 import { CustomButton } from "./components/html/Button";
+import { Text } from "./components/polymorphic/Text";
 
 function App() {
   // const personName = {
@@ -111,10 +112,19 @@ function App() {
         onClick={item => console.log(item)}
       />
       <RandomNumber value={10} isPositive />
-      <Toast position='center' /> */}
+      <Toast position='center' />
       <CustomButton variant='primary' onClick={() => console.log('Clicked')}>
         Button Label
-      </CustomButton>
+      </CustomButton> */}
+      <Text size='lg' as='h1'>
+        Heading
+      </Text>
+      <Text size='md' as='p'>
+        Paragraph
+      </Text>
+      <Text size='sm' color='secondary' as='label' htmlFor='someId'>
+        Label
+      </Text>
     </>
   );
 }
